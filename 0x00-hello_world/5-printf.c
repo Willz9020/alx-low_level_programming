4ddc7e747e4d0b203653538d80b9adfd84c953fe
Godswill <godswillmfon51@gmail.com>
2022-10-13 16:50:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-nclude <stdio.h>
+#include <studio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main -Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n";
 	return (0);
 }
