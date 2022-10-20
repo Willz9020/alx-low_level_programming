7a5a8f6b32edc7cff0def49735e87a38d78357d9
Godswill <godswillmfon51@gmail.com>
2022-10-19 22:33:33 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
