257d07b11c57fea11767a20f969ed7484f196128
Godswill <godswillmfon51@gmail.com>
2022-10-19 23:20:35 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,12 @@ void print_alphabet_x10(void)
 {
 	int n, co;
 
-	while (co < 10)
+	while (co <= 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
 		}
-		co++;
 		_putchar('\n');
 	}
 }
