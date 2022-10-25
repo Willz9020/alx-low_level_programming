da59b3e96dca7fcd5ccbbc588511b70a6ac0c811
Godswill <godswillmfon51@gmail.com>
2022-10-24 19:31:30 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int k = 0;
+
+	while (s[k] != '\0')
+		k++;
+	for (k = k - 1; k >= 0; k--)
+		_putchar(s[k]);
+
+	_putchar('\n')
+}
