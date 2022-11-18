ae6eff87bee349a1e519a83f685ee4a7f6b1f8e3
Godswill <godswillmfon51@gmail.com>
2022-11-17 18:29:16 -0800

    done deal

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
+/**
+ * File: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0F-variadic_functions directory.
+ */
+
 #include <stdarg.h>
 
 /**
