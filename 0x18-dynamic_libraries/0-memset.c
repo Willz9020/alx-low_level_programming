1fc588a61971c1e0f397b27114d121084d47a89f
Godswill <godswillmfon51@gmail.com>
2023-01-19 05:11:38 -0800

    dynamic library libdynamic.so

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte.
+ * _memset - fills memory with a constant byte.
  * @s: pointer to put the constant
- * @b: constant
+ * @b: constant
  * @n: max bytes to use
  * Return: s
  */
